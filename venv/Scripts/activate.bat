ae85ccbc369be6e48173ec2e7a92a36e4624cd46
juliancelso <juliancelsoginzburg@gmail.com>
2025-02-07 09:11:24 -0300

    Added requirements.txt with project dependencies

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\pelul\OneDrive\Escritorio\sistema_historias\sistema_historias_backend\venv
+set VIRTUAL_ENV=C:\Users\Ana y Juli\Desktop\bastaporfavor\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
